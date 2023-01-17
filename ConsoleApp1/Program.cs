878d9301cdd1a3552cc86098e782dd3bc533b88e
Thiago Menezes <thiagocwbpr@gmail.com>
2023-01-17 12:20:03 -0300

    Criado função que armazenará o menu principal da calculadora. Achei melhor criar uma função, para trabalhar de forma mais organizada no código.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Menu();
+
+static void Menu() {
+
+    Console.Clear();
+    
+Console.WriteLine("\nCALCULADORA DE TERMINAL\n");
+Console.WriteLine("1 - SOMA");
+Console.WriteLine("2 - SUBTRAÇÃO");
+Console.WriteLine("3 - MULTIPLICAÇÃO");
+Console.WriteLine("4 - DIVISÃO");
+Console.WriteLine("5 - SAIR");
+Console.Write("\nSELECIONE UMA DAS OPÇÕES NUMÉRICAS ACIMA: ");
+
+    Console.ReadKey();
+}
