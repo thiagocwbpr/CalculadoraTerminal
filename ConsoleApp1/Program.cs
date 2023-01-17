4676c0556d203083324dd9d57ec35fa557a119e6
Thiago Menezes <thiagocwbpr@gmail.com>
2023-01-17 13:39:25 -0300

    Criado a condição de acesso as funções. Portanto avaliando o código, achei melhor utilizar um SWITCH do que vários IF e ELSE. Então, o SWITCH está fazendo a função de acesso entre o MENU PRINCIPAL e as demais funções para calculos.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Globalization;
 
 Menu();
 
@@ -6,89 +6,117 @@ static void Menu() {
 
     Console.Clear();
     
-Console.WriteLine("\nCALCULADORA DE TERMINAL\n");
-Console.WriteLine("1 - SOMA");
-Console.WriteLine("2 - SUBTRAÇÃO");
-Console.WriteLine("3 - MULTIPLICAÇÃO");
-Console.WriteLine("4 - DIVISÃO");
-Console.WriteLine("5 - SAIR");
-Console.Write("\nSELECIONE UMA DAS OPÇÕES NUMÉRICAS ACIMA: ");
+    Console.WriteLine("\nCALCULADORA DE TERMINAL\n");
+    Console.WriteLine("1 - SOMA");
+    Console.WriteLine("2 - SUBTRAÇÃO");
+    Console.WriteLine("3 - MULTIPLICAÇÃO");
+    Console.WriteLine("4 - DIVISÃO");
+    Console.WriteLine("5 - PORCENTAGEM");
+    Console.WriteLine("6 - SAIR");
+    Console.Write("\nSELECIONE UMA DAS OPÇÕES NUMÉRICAS ACIMA: ");
+    short opcao = short.Parse(Console.ReadLine());
+
+    switch (opcao) {
+        case 1:
+            soma();
+            break;
+        case 2:
+            subtracao();
+            break;
+        case 3:
+            multiplicacao();
+            break;
+        case 4:
+            divisao();
+            break;
+        case 5:
+            porcentagem();
+            break;
+        case 6:
+            Console.Clear();
+            Console.WriteLine("\n\nObrigado por utilizar a calculadora de terminal! Até a próxima!");
+            break;
+            System.Environment.Exit(0);
+        default:
+            Menu();
+            break;
+    }
 
     Console.ReadKey();
 
 }
 static void soma() {
     Console.Clear();
-    Console.WriteLine(" \nVOCÊ DESEJA REALIZAR UMA SOMA \n");
-    Console.Write("Digite o primeiro numero a ser calculado: \n");
+    Console.Write(" \nVOCÊ DESEJA REALIZAR UMA SOMA \n\n");
+    Console.Write("\nDigite o primeiro numero a ser calculado: ");
     float n1 = float.Parse(Console.ReadLine());
-    Console.Write("Digite o segundo numero a ser calculado: \n");
+    Console.Write("\nDigite o segundo numero a ser calculado: ");
     float n2 = float.Parse(Console.ReadLine());
 
     float n3 = n1 + n2;
 
-    Console.WriteLine($"O resultado da soma entre {n1} e {n2} é : {n3}\n");
+    Console.WriteLine($"\n\nO resultado da soma entre {n1} e {n2} é : {n3}\n");
     Console.Write("\nPressione qualquer tecla para retornar ao menu principal.");
     Console.ReadKey();
     Menu();
 }
 static void subtracao() {
     Console.Clear();
-    Console.WriteLine(" \nVOCÊ DESEJA REALIZAR UMA SUBTRAÇÃO \n");
-    Console.Write("Digite o primeiro numero a ser calculado: \n");
+    Console.Write(" \nVOCÊ DESEJA REALIZAR UMA SUBTRAÇÃO \n\n");
+    Console.Write("\nDigite o primeiro numero a ser calculado: ");
     float n1 = float.Parse(Console.ReadLine());
-    Console.Write("Digite o segundo numero a ser calculado: \n");
+    Console.Write("\nDigite o segundo numero a ser calculado: ");
     float n2 = float.Parse(Console.ReadLine());
 
     float n3 = n1 - n2;
 
-    Console.WriteLine($"O resultado da subtração entre {n1} e {n2} é : {n3}\n");
+    Console.WriteLine($"\n\nO resultado da subtração entre {n1} e {n2} é : {n3}\n");
     Console.Write("\nPressione qualquer tecla para retornar ao menu principal.");
     Console.ReadKey();
     Menu();
 }
 static void multiplicacao() {
     Console.Clear();
-    Console.WriteLine(" \nVOCÊ DESEJA REALIZAR UMA MULTIPLICAÇÃO \n");
-    Console.Write("Digite o primeiro numero a ser calculado: \n");
+    Console.Write(" \nVOCÊ DESEJA REALIZAR UMA MULTIPLICAÇÃO \n\n");
+    Console.Write("\nDigite o primeiro numero a ser calculado: ");
     float n1 = float.Parse(Console.ReadLine());
-    Console.Write("Digite o segundo numero a ser calculado: \n");
+    Console.Write("\nDigite o segundo numero a ser calculado: ");
     float n2 = float.Parse(Console.ReadLine());
 
     float n3 = n1 * n2;
 
-    Console.WriteLine($"O resultado da multiplicação entre {n1} e {n2} é : {n3}\n");
+    Console.WriteLine($"\n\nO resultado da multiplicação entre {n1} e {n2} é : {n3}\n");
     Console.Write("\nPressione qualquer tecla para retornar ao menu principal.");
     Console.ReadKey();
     Menu();
 }
 static void divisao() {
     Console.Clear();
-    Console.WriteLine(" \nVOCÊ DESEJA REALIZAR UMA DIVISÃO \n");
-    Console.Write("Digite o primeiro numero a ser calculado: \n");
+    Console.Write(" \nVOCÊ DESEJA REALIZAR UMA DIVISÃO \n\n");
+    Console.Write("\nDigite o primeiro numero a ser calculado: ");
     float n1 = float.Parse(Console.ReadLine());
-    Console.Write("Digite o segundo numero a ser calculado: \n");
+    Console.Write("\nDigite o segundo numero a ser calculado: ");
     float n2 = float.Parse(Console.ReadLine());
 
     float n3 = n1 / n2;
 
-    Console.WriteLine($"O resultado da divisão entre {n1} e {n2} é : {n3}\n");
+    Console.WriteLine($"\n\nO resultado da divisão entre {n1} e {n2} é : {n3}\n");
     Console.Write("\nPressione qualquer tecla para retornar ao menu principal.");
     Console.ReadKey();
     Menu();
 }
 static void porcentagem() {
     Console.Clear();
-    Console.WriteLine(" \nVOCÊ DESEJA REALIZAR UMA PORCENTAGEM \n");
-    Console.Write("Digite o valor a ser calculado R$:  \n");
+    Console.Write(" \nVOCÊ DESEJA REALIZAR UMA PORCENTAGEM \n\n");
+    Console.Write("\nDigite o valor a ser calculado R$: ");
     float n1 = float.Parse(Console.ReadLine());
-    Console.Write("Digite a porcentagem que deseja calcular %: \n");
+    Console.Write("\nDigite a porcentagem que deseja calcular %: ");
     float n2 = float.Parse(Console.ReadLine());
 
     float n3 = n2 * n1;
     float n4 = n3 / 100;
 
-    Console.WriteLine($" {n2}% de R${n1} Reais é : R${n3} Reais \n");
+    Console.WriteLine($"\n\n {n2}% de R${n1.ToString("F2", CultureInfo.InvariantCulture)} Reais é : R${n4.ToString("F2",CultureInfo.InvariantCulture)} Reais \n");
     Console.Write("\nPressione qualquer tecla para retornar ao menu principal.");
     Console.ReadKey();
     Menu();
