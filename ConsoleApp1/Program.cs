65c03685f5fe687b89d6bb9bddbb03fa39147b43
Thiago Menezes <thiagocwbpr@gmail.com>
2023-01-17 12:40:13 -0300

    Criado função soma. Nessa função, após realizar o calculo de soma, ela informa o usuario para pressionar qualquer tecla, para retornar ao menu Principal da Calculadora.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,4 +15,21 @@ Console.WriteLine("5 - SAIR");
 Console.Write("\nSELECIONE UMA DAS OPÇÕES NUMÉRICAS ACIMA: ");
 
     Console.ReadKey();
+
+}
+
+static void soma() {
+    Console.Clear();
+    Console.WriteLine(" \nVOCÊ DESEJA REALIZAR UMA SOMA \n");
+    Console.Write("Digite o primeiro numero a ser calculado: \n");
+    float n1 = float.Parse(Console.ReadLine());
+    Console.Write("Digite o segundo numero a ser calculado: \n");
+    float n2 = float.Parse(Console.ReadLine());
+
+    float n3 = n1 + n2;
+
+    Console.WriteLine($"O resultado da soma entre {n1} e {n2} é : {n3}\n");
+    Console.Write("\nPressione qualquer tecla para retornar ao menu principal.");
+    Console.ReadKey();
+    Menu();
 }
