345f6ad5cfb89f7bcc3c6b1213d2a27f5ef16860
Thiago Menezes <thiagocwbpr@gmail.com>
2023-01-17 12:56:41 -0300

    Criado as demais funções da Calculadora. Subtração, multiplicação, divisão e também a função para calculo de porcentagem.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -33,3 +33,64 @@ static void soma() {
     Console.ReadKey();
     Menu();
 }
+static void subtracao() {
+    Console.Clear();
+    Console.WriteLine(" \nVOCÊ DESEJA REALIZAR UMA SUBTRAÇÃO \n");
+    Console.Write("Digite o primeiro numero a ser calculado: \n");
+    float n1 = float.Parse(Console.ReadLine());
+    Console.Write("Digite o segundo numero a ser calculado: \n");
+    float n2 = float.Parse(Console.ReadLine());
+
+    float n3 = n1 + n2;
+
+    Console.WriteLine($"O resultado da subtração entre {n1} e {n2} é : {n3}\n");
+    Console.Write("\nPressione qualquer tecla para retornar ao menu principal.");
+    Console.ReadKey();
+    Menu();
+}
+static void multiplicacao() {
+    Console.Clear();
+    Console.WriteLine(" \nVOCÊ DESEJA REALIZAR UMA MULTIPLICAÇÃO \n");
+    Console.Write("Digite o primeiro numero a ser calculado: \n");
+    float n1 = float.Parse(Console.ReadLine());
+    Console.Write("Digite o segundo numero a ser calculado: \n");
+    float n2 = float.Parse(Console.ReadLine());
+
+    float n3 = n1 + n2;
+
+    Console.WriteLine($"O resultado da multiplicação entre {n1} e {n2} é : {n3}\n");
+    Console.Write("\nPressione qualquer tecla para retornar ao menu principal.");
+    Console.ReadKey();
+    Menu();
+}
+static void divisao() {
+    Console.Clear();
+    Console.WriteLine(" \nVOCÊ DESEJA REALIZAR UMA DIVISÃO \n");
+    Console.Write("Digite o primeiro numero a ser calculado: \n");
+    float n1 = float.Parse(Console.ReadLine());
+    Console.Write("Digite o segundo numero a ser calculado: \n");
+    float n2 = float.Parse(Console.ReadLine());
+
+    float n3 = n1 + n2;
+
+    Console.WriteLine($"O resultado da divisão entre {n1} e {n2} é : {n3}\n");
+    Console.Write("\nPressione qualquer tecla para retornar ao menu principal.");
+    Console.ReadKey();
+    Menu();
+}
+static void porcentagem() {
+    Console.Clear();
+    Console.WriteLine(" \nVOCÊ DESEJA REALIZAR UMA PORCENTAGEM \n");
+    Console.Write("Digite o valor a ser calculado R$:  \n");
+    float n1 = float.Parse(Console.ReadLine());
+    Console.Write("Digite a porcentagem que deseja calcular %: \n");
+    float n2 = float.Parse(Console.ReadLine());
+
+    float n3 = n2 * n1;
+    float n4 = n3 / 100;
+
+    Console.WriteLine($" {n2}% de R${n1} Reais é : R${n3} Reais \n");
+    Console.Write("\nPressione qualquer tecla para retornar ao menu principal.");
+    Console.ReadKey();
+    Menu();
+}
